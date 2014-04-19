10410a1fdffdfb9e18f80794f4c9540e87a22c97
Yuri Pourre <yuriamayuri@gmail.com>
2014-04-19 11:54:59 -0300

    Add i18n stuff

---
+++ EtyllicaMobile/src/br/com/etyllica/i18n/LanguageAdapter.java
@@ -0,0 +1,43 @@
+package br.com.etyllica.i18n;
+
+import java.util.Map;
+
+public class LanguageAdapter {
+
+	private Map<String, Language> languages;
+	
+	public LanguageAdapter() {
+		super();
+		
+		languages.put("en", Language.ENGLISH_USA);
+		languages.put("pt", Language.PORTUGUESE_BRAZIL);
+		languages.put("ja", Language.JAPANESE);
+		
+		//Display Languages
+		//cs
+		//de(utch)
+		//en(glish)
+		//es(panol)
+		//fr(ench)
+		//it(alia)
+		//ko(rean)
+		//ja(pan)
+		//ru(ssian)
+		//pt
+		
+	}
+	
+	public Language getLanguage(String locale) {
+		
+		if(languages.containsKey(locale)){
+			
+			return languages.get(locale);
+			
+		} else {
+		
+			return Language.ENGLISH_USA;
+		}
+	}
+	
+	
+}
